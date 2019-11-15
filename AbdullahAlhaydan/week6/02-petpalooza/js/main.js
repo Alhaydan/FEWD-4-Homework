1d3d08856caf3a77ec642fe990f7dc36206c3dff
Alhaydan <a.alhaydan11@gmail.com>
2019-11-16 01:40:30 +0300

    A

---
+++ AbdullahAlhaydan/week6/02-petpalooza/js/main.js
@@ -0,0 +1,9 @@
+function showImage(){
+    document.getElementById('loadingImage').style.visibility="visible";
+    document.getElementById('loadingImage1').style.visibility="visible";
+    document.getElementById('loadingImage2').style.visibility="visible";
+    document.getElementById('loadingImage3').style.visibility="visible";
+    document.getElementById('loadingImage4').style.visibility="visible";
+
+
+}
